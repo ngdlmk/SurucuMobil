a1e24f1f2fbe4d13f952457cbcb99b22fd08662f
Reşit PAYÇIN <rpaycin@gmail.com>
2019-07-13 08:22:35 +0300

    login çalışmaları

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,7 @@
 import React,{Component} from "react";
-import { View, Text,Button, AsyncStorage  } from "react-native";
+import { View, Alert, Image, Text, ActivityIndicator,AsyncStorage } from 'react-native';
+import { Container, Header, Content, Item, Input, Icon, Button } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 
 var NavigateKeys=require('../data/NavigateKeys.json');
 var StorageKeys=require('../data/StorageKeys.json');
@@ -7,6 +9,12 @@ var StorageKeys=require('../data/StorageKeys.json');
 export default class LoginScreen extends Component {
     constructor(props){
       super(props);
+
+      this.state = {
+        userName: "",
+        userPassword: "",
+        animateLogin: false
+      };
     }
 
     componentWillMount(){
@@ -19,14 +27,67 @@ export default class LoginScreen extends Component {
     }
 
   render() {
+    const isLoading = this.state.animateLogin;
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Login Screen</Text>
-        <Button
-          title="Anasayfaya git"
-          onPress={this.loginOperation.bind(this)}
-        />
-      </View>
+      <Container>
+      <Content style={{ paddingLeft: 5, paddingRight: 5 }}>
+          <Grid style={{ marginTop: 70 }}>
+              <Row size={40} style={{ marginBottom: 40 }}>
+                  <Col size={100} style={{ alignContent: "center", alignItems: "center" }}>
+                      <Image source={require('../../assets/bmsLoginLogo.png')} />
+                  </Col>
+              </Row>
+              <View style={{ justifyContent: 'space-around', }}>
+                  {isLoading && (
+                      <ActivityIndicator
+                          animating={this.state.animateLogin}
+                          style={{ height: 80 }}
+                          color="#0000ff"
+                          size="large"
+                          hidesWhenStopped={true}
+                      />
+                  )}
+              </View>
+              <Row size={20} style={{ marginBottom: 5 }}>
+                  <Col size={100}>
+                      <Item>
+                          <Icon style={{ color: '#E9E5E4', padding: 0 }} name="ios-document"></Icon>
+                          <Input value={this.state.userName} keyboardType="email-address"
+                              onChangeText={(value) => this.setState({ userName: value })}
+                              placeholder='Email' />
+                      </Item>
+                  </Col>
+              </Row>
+              <Row size={20} style={{ marginBottom: 15 }}>
+                  <Col size={100}>
+                      <Item>
+                          <Icon style={{ color: '#E9E5E4', padding: 0 }} name="lock"></Icon>
+                          <Input secureTextEntry={true} onChangeText={(value) => this.setState({ userPassword: value })}
+                              value={this.state.userPassword} 
+                              placeholder='Şifre' />
+                      </Item>
+                  </Col>
+              </Row>
+          </Grid>
+          <Grid tyle={{ paddingLeft: 5, paddingRight: 5, paddingTop: 5 }}>
+              <Row size={20} style={{ alignContent: "center", alignItems: "center" }}>
+                  <Col size={50} >
+                      <Button block rounded light
+                          onPress={this.loginOperation.bind(this)}>
+                          <Text>Giriş</Text>
+                      </Button>
+                  </Col>
+              </Row>
+          </Grid>
+          <Grid style={{ marginTop: 150 }}>
+              <Row size={100}>
+                  <Col size={100} style={{ alignContent: "center", alignItems: "center" }}>
+                      <Image style={{ width: 150, height: 38 }} source={require('../../assets/ceturlogo.png')} />
+                  </Col>
+              </Row>
+          </Grid>
+      </Content>
+  </Container>
     );
   }
 
